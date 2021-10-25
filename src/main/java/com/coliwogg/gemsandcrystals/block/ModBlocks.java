d8b48db32ed414c7f7d3b0863e0578738d6d095f
Colin <coliwogg@outlook.com>
2021-10-25 11:23:17 -0300

    Added Block of Ruby, Crafting and Smelting Recipes, and Loot Tables

--- src/main/java/com/coliwogg/gemsandcrystals/block/ModBlocks.java
+++ src/main/java/com/coliwogg/gemsandcrystals/block/ModBlocks.java
@@ -6,26 +6,37 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
-    public static final Block RUBY_ORE = registerBlock("ruby_ore",
-            new Block(FabricBlockSettings.of(Material.STONE).strength(4.0F)
-                    .breakByTool(FabricToolTags.PICKAXES, 2).requiresTool()));
+    public static final Block RUBY_ORE;
+    public static final Block RUBY_BLOCK;
+
+    static {
+        RUBY_ORE = registerBlock("ruby_ore",
+                new OreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(3.0F, 3.0F)
+                        .breakByTool(FabricToolTags.PICKAXES, 2), UniformIntProvider.create(3, 7)));
+        RUBY_BLOCK = registerBlock("ruby_block",
+                new Block(FabricBlockSettings.of(Material.METAL, MapColor.BRIGHT_RED).requiresTool().strength(5.0F, 6.0F)
+                        .sounds(BlockSoundGroup.METAL).breakByTool(FabricToolTags.PICKAXES, 2)));
+    }
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registry.BLOCK, new Identifier(GemsAndCrystals.MOD_ID, name), block);
     }
 
-    private static Item registerBlockItem(String name, Block block) {
-        return Registry.register(Registry.ITEM, new Identifier(GemsAndCrystals.MOD_ID, name),
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registry.ITEM, new Identifier(GemsAndCrystals.MOD_ID, name),
                 new BlockItem(block, new FabricItemSettings().group(ModItemGroup.GEMS_AND_CRYSTALS)));
     }
 
