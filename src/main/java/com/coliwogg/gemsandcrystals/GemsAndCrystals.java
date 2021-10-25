5737225280a1a0738a3c66063647d0b664981f31
Colin <coliwogg@outlook.com>
2021-10-25 10:55:33 -0300

    Added Ruby Item and Ruby Ore Block

--- src/main/java/com/coliwogg/gemsandcrystals/GemsAndCrystals.java
+++ src/main/java/com/coliwogg/gemsandcrystals/GemsAndCrystals.java
@@ -1,21 +1,29 @@
 package com.coliwogg.gemsandcrystals;
 
+import com.coliwogg.gemsandcrystals.block.ModBlocks;
+import com.coliwogg.gemsandcrystals.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class GemsAndCrystals implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("gemsandcrystals");
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    public static final String MOD_ID = "gemsandcrystals";
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LogManager.getLogger("gemsandcrystals");
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+		ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
+
+        LOGGER.info("Hello Fabric world!");
+    }
 }
---
+++ src/main/java/com/coliwogg/gemsandcrystals/item/ModItems.java
@@ -0,0 +1,23 @@
+package com.coliwogg.gemsandcrystals.item;
+
+import com.coliwogg.gemsandcrystals.GemsAndCrystals;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item RUBY = registerItem("ruby",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(GemsAndCrystals.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        GemsAndCrystals.LOGGER.info("Registering ModItems for " + GemsAndCrystals.MOD_ID);
+    }
+
+}
